503f935bd8367d45c7be043c0d53dc091a0375c2
tahaxtrex <hbirritaha@gmail.com>
2025-07-22 15:25:14 +0100

    init pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react"
+import Navbar from "./components/navbar.jsx"
+import { Route, Routes } from "react-router-dom"
+
+import Home from "./pages/homepage.jsx"
+import SignupPage from "./pages/signuppage.jsx"
+import LoginPage from "./pages/loginpage.jsx"
+import SettingsPage from "./pages/settingpage.jsx"
+import ProfilePage from "./pages/profilepage.jsx"
 
 const App = () => {
   return (
     <div>
-      HELLO WORLD!
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-600">
+      <p className="">navbar</p>
+    </div>
+  )
+}
+
+export default Navbar
