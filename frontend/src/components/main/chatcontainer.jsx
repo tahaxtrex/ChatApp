8c6ec54b82d899503a3d246097b257ed784daac8
tahaxtrex <hbirritaha@gmail.com>
2025-08-01 23:16:47 +0100

    small change

--- frontend/src/components/main/chatcontainer.jsx
+++ frontend/src/components/main/chatcontainer.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 function Chatcontainer() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='flex flex-col h-full'>
+        <div className='flex-1 overflow-y-auto'>
+          <div className='flex flex-col gap-4 p-4'>
+            {messages.map((message)=>(
+              <div key={message._id}>{message.content}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
